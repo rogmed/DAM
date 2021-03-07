7fd1fe9f5fca83a3e62af2e828208be7e26f2d13
rogmed <73010972+rogmed@users.noreply.github.com>
2021-03-07 04:27:27 +0100

    Ejercicio05 main

---
+++ WorkSpaceJava/src/actividad05/main/Ejercicio01.java
@@ -0,0 +1,20 @@
+package actividad05.main;
+
+import actividad05.*;
+
+public class Ejercicio01 {
+
+    public static void main(String[] args){
+        int opcion = pideEntero("Elija una opcion:"
+        +"\n1- Muestra Pi."
+        +"\n2- Muestra valor aleatorio."
+        +"\n3- "
+        +"\n4- "
+        +"\n5- Muestra raiz cuadrada."
+        +"\n6- Calcula potencia."
+        +"\nO- Salir."
+        +"\nOpcion: ");
+
+        System.out.println("Opcion elegida: "+opcion);
+    }
+}
